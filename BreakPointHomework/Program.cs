4a77a434b02af00ba2bf665282645b519e4e2ee6
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:29:26 +0200

    add app

---
+++ BreakPointHomework/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+static void Runs100Times()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (i == 73)
+        {
+            throw new Exception($"The counter is {i}");
+        }
+    }
+}
+
+try
+{
+    Runs100Times();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
